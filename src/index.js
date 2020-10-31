538080f2a39aea0290e685a24e1b1b9eb67f1795
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-10-31 02:05:09 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const formField = document.querySelector("#create-task-form");
+  const todoList = document.querySelector("#tasks");
+  const description = document.querySelector("#new-task-description");
+
+
+  formField.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log("submitted");
+    createTask();
+  })
+
+  function createTask() {
+    let li = document.createElement("li");
+    li.innerHTML = description.value;
+    
+    todoList.append(li);
+    formField.reset();
+  }
 });
